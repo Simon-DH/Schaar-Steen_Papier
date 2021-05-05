75794dcc7f9c8614caa6b0842a46807e16c6e580
Simon-DH <simondh-github@rekeningen.pdhlh.eu>
2021-05-05 20:41:11 +0200

    begin van spelronde

--- schaar_steen_papier.js
+++ schaar_steen_papier.js
@@ -1,5 +1,8 @@
 console.log("Hello, World! Dit is een spelletje Schaar Steen Papier in de console van je browser")
 
+/* 3) Your game is going to play against the computer, 
+so begin with a function called computerPlay 
+that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. */
 // de computer moet ad-random elke keer iets kiezen: papier, schaar of steen
 function computerPlay() {
     //Wat zijn de mogelijkheden
@@ -8,7 +11,16 @@ function computerPlay() {
     return mogelijkheden[Math.floor(Math.random()*3)];
 }
 
+/* Write a function that plays a single round of Rock Paper Scissors. 
+The function should take two parameters - 
+the playerSelection and computerSelection - 
+and then return a string that declares the winner of the round
+zorg dat case insensiteve ingegeven kan worden */
 
+function playRound(playerSelection, computerSelection) {
+    
+
+}
 
 // we vergelijken de uitkomst van de speler met die van de computer
 // de winaar krijgt een punt
